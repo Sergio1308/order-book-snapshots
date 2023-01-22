ede7b600b7215e67b197ea51807b545b362397f4
Sergei <sergi.haiduk@gmail.com>
2023-01-22 13:16:38 +0200

    rewritten methods for working with maps

--- src/main/java/org/example/models/OrderBook.java
+++ src/main/java/org/example/models/OrderBook.java
@@ -1,5 +1,6 @@
 package org.example.models;
 
+import java.util.Locale;
 import java.util.Map;
 
 public class OrderBook {
@@ -13,11 +14,57 @@ public class OrderBook {
     }
 
     /**
+     * Difference in price between the previous and current request.
+     * Checks if the price of the current order book (bids/asks) was updated or added compared to previous order book.
+     * @param previousBookMap map, contains previous bids or asks.
+     * @param currentBook current bid/ask object to check.
+     * @return result string.
+     */
+    public String checkIfPriceLevelUpdatedOrAdded(Map<Double, ? extends AbstractBook> previousBookMap,
+                                                  AbstractBook currentBook) {
+        if (!previousBookMap.containsKey(currentBook.getPrice())) {
+            return String.format(
+                    Locale.US, "new [%s] (%.3f, %.3f)",
+                    currentBook.getSide(),
+                    currentBook.getPrice(),
+                    currentBook.getQty());
+        } else {
+            if (previousBookMap.get(currentBook.getPrice()).getQty() != currentBook.getQty()) {
+                return String.format(
+                        Locale.US,"update [%s] (%.3f, %.3f)",
+                        currentBook.getSide(),
+                        currentBook.getPrice(),
+                        currentBook.getQty());
+            }
+        }
+        return "";
+    }
+
+    /**
+     * Difference in price between the previous and current book.
+     * Checks if the price of the previous order book (bids/asks) exists in current order book.
+     * @param currentBookMap map, contains current bids or asks.
+     * @param previousBook previous bid/ask object to check.
+     * @return result string.
+     */
+    public String checkIfPriceLevelDeleted(Map<Double, ? extends AbstractBook> currentBookMap,
+                                           AbstractBook previousBook) {
+        if (!currentBookMap.containsKey(previousBook.getPrice())) {
+            return String.format(
+                    Locale.US,"delete [%s] (%.3f, %.3f)",
+                    previousBook.getSide(),
+                    previousBook.getPrice(),
+                    previousBook.getQty());
+        }
+        return "";
+    }
+
+    /**
      * Calculate qty (size) difference between previous order book and current.
      * @param previousOrderBook previous order book, contains previous bids and asks.
      * @return array of double, where index 0 - bids difference, index 1 - asks difference.
      */
-    public double[] calculateQtyDifferenceBetweenOrders(OrderBook previousOrderBook) {
+    public double[] calculateQtyDifferenceBetweenOrderBooks(OrderBook previousOrderBook) {
         double currentBidsSum = sumOfMapQty(bidsMap);
         double currentAsksSum = sumOfMapQty(asksMap);
         double previousBidsSum = sumOfMapQty(previousOrderBook.bidsMap);
